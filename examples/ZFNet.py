d04b745990cb9a601f2e3dc0dda34492eb846ccf
yusuke-a-uchida <yusuke.a.uchida@dena.com>
2018-01-04 01:04:17 +0900

    add ZFNet

---
+++ examples/ZFNet.py
@@ -0,0 +1,25 @@
+import os
+import sys
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+from convnet_drawer import Model, Conv2D, MaxPooling2D, Flatten, Dense
+
+
+def main():
+    model = Model(input_shape=(224, 224, 3))
+    model.add(Conv2D(96, (7, 7), strides=(2, 2)))
+    model.add(MaxPooling2D((3, 3), strides=(2, 2)))
+    model.add(Conv2D(256, (5, 5), strides=(2, 2), padding="same"))
+    model.add(MaxPooling2D((3, 3), strides=(2, 2)))
+    model.add(Conv2D(384, (3, 3), padding="same"))
+    model.add(Conv2D(384, (3, 3), padding="same"))
+    model.add(Conv2D(256, (3, 3), padding="same"))
+    model.add(MaxPooling2D((3, 3), strides=(2, 2)))
+    model.add(Flatten())
+    model.add(Dense(4096))
+    model.add(Dense(4096))
+    model.add(Dense(1000))
+    model.save_fig(os.path.splitext(os.path.basename(__file__))[0] + ".svg")
+
+
+if __name__ == '__main__':
+    main()
